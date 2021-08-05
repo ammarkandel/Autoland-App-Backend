687972449831d99f2428dbe7e196ab11afd24224
Ammar Kandel <merom8703@gmail.com>
2021-08-05 02:43:38 +0200

    Add comment to authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+  # disable authenticate_user until manage SSL Certificate
   # before_action :authenticate_user
 end
